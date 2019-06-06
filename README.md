0b3b3bed49c0f0194086a74b2b4965d40e431a35
hhtnghia321 <nghiahht1902@gmail.com>
2019-06-06 20:21:25 +0700

    2nd

--- README.md
+++ README.md
@@ -8,12 +8,29 @@ This is the original repository have all of the requested file for the course cr
 4. I also include the **Peergrade-assignment.Rproj** for those who want to fork my project in order to examine, edit or do what every they want continuely 
 
 ## Dataset abstract
+> The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
 > you can get your download of the dataset file **[Here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)**
 unzip file to get the *UCI HAR Data* folder which contain all of the untidy data. The folder contain:
 1. The **README.txt**: explain about the experiment, how they measure the experiment observation and more information about other file
 2. The **Test** and **Train** folder containt the primary data
 
 ## Simple exmaplaination for the run_analysis.R file
+1. download all needed R packages for the analysis (dplyr)
+2. load the package
+3. Download the **Dataset.zip** file for the  project
+4. unzip the file
+5. read all the needed file:
+  - *X_train.txt*, *X_test.txt* for the data
+  - *y_train.txt*, *y_test.txt* for the Activities code
+  - *subject_train.txt*, *subject_text.txt* for the subject index
+  - *feature.txt* for the varibles name
+  - *activity_labels.txt* for the activities name with corresponding code
+6. Merge the train and test data with correponding activity code and subject index
+7. rename the activity code with its name base on the *activity_labels.txt* file
+8. rename each shortened words in original variables name with the its full name for more comprehensive view
+9. recreate a independent table which contain the mean of each varibles for each activity and subject
+10. Export the independent file into the **Tidydataset.txt**
+11. Delete all of the unnecessary objects
 
 ## Variable Desciption
 > the description is presented in the Codebook.md
--- run_analysis.R
+++ run_analysis.R
@@ -7,7 +7,7 @@ if(packageVersion("dplyr") == Null){
 library(dplyr)
 
 #download file 
-if(!file.exists(Dataset.zip)){
+if(!file.exists("Dataset.zip")){
   download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
                 destfile = paste0(getwd(), "/Dataset.zip"), method = "curl")
 }
@@ -20,12 +20,12 @@ if(!file.exists("UCI HAR Dataset")){
 Features <- read.table(paste0(getwd(), "/UCI HAR Dataset/features.txt"), col.names = c("n", "features")) 
 
 #read train data
-Label_train <-read.table(paste0(getwd(), "/UCI HAR Dataset/train/y_train.txt"), col.names = "featurecode" )
+Label_train <-read.table(paste0(getwd(), "/UCI HAR Dataset/train/y_train.txt"), col.names = "activitycode" )
 Set_train <-read.table(paste0(getwd(), "/UCI HAR Dataset/train/X_train.txt"), col.names = Features$features)
 Subj_train <- read.table(paste0(getwd(), "/UCI HAR Dataset/train/subject_train.txt"), col.names ="Subjects" )
 
 #read test data
-Label_test <-read.table(paste0(getwd(), "/UCI HAR Dataset/test/y_test.txt"),col.names = "featurecode") 
+Label_test <-read.table(paste0(getwd(), "/UCI HAR Dataset/test/y_test.txt"),col.names = "activitycode") 
 Set_test <-read.table(paste0(getwd(), "/UCI HAR Dataset/test/X_test.txt"), col.names = Features$features)
 Subj_test <- read.table(paste0(getwd(), "/UCI HAR Dataset/test/subject_test.txt"), col.names ="Subjects")
 
@@ -39,10 +39,10 @@ Subj <- rbind(Subj_train, Subj_test)
 Total1 <- cbind(Subj, Label, Set)
 
 #Name activities
-Total1$featurecode <- Activities[Total1$featurecode, 2]
+Total1$activitycode <- Activities[Total1$activitycode, 2]
 
 #rename the activities colum
-names(Total1)[names(Total1) == "featurecode"] <- "Activities"
+names(Total1)[names(Total1) == "activitycode"] <- "Activities"
 
 
 #Extract only the mean and standard deviation
@@ -53,15 +53,24 @@ colnames(Total) <- sub("^t", "Time", colnames(Total))
 colnames(Total) <- sub("^f", "Fourier", colnames(Total))
 colnames(Total) <- sub("Acc", "Acceleration", colnames(Total))
 colnames(Total) <- sub("Gyro", "Gyroscope", colnames(Total))
-colnames(Total) <- sub("/.mean()", "-Mean", colnames(Total))
-colnames(Total) <- sub("/.std()", "-StandardDeviation", colnames(Total))
+colnames(Total) <- sub(".mean", "-Mean", colnames(Total))
+colnames(Total) <- sub(".std", "-StandardDeviation", colnames(Total))
 colnames(Total) <- sub("Mag", "Magnitude", colnames(Total))
 colnames(Total) <- sub("BodyBody", "Body", colnames(Total))
 colnames(Total) <- sub("Freq", "Frequency", colnames(Total))
-colnames(Total) <- sub("/.(/.){,1}/.", ".", colnames(Total))
+colnames(Total) <- gsub("(\\.){1,3}", "-", colnames(Total))
+colnames(Total) <- gsub("-$", "", colnames(Total))
 colnames(Total) <- sub("angle", "Angle", colnames(Total))
 colnames(Total) <- sub("gravity", "Gravity", colnames(Total))
 
 #create independent table that have 
-Tidydataset <- Total %>% group_by(Activities, Subjects) %>% summarize_all(list(mean))
-write.table(Tidydataset,"Tidydataset.txt")
+Tidydataset <- Total %>% group_by(Activities, Subjects) %>% summarize_all(mean)
+if(!file.exists("Tidydataset.txt")){
+   write.table(Tidydataset,"Tidydataset.txt")
+  }
+   
+# remove the unnecessary objects
+rm(Activities, Features, Label, Label_test, Label_train, Set, Set_test, Set_train, Subj, Subj_test, Subj_train, Total1)
+
+   
+   
